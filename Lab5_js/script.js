3b7735f9e37c39510ee346520b2388706ea2573a
Alat234 <atavlad20052@gmail.com>
2025-04-28 23:17:47 +0300

    create file for LightsOut game

--- Lab5_js/script.js
+++ Lab5_js/script.js
@@ -1,4 +1,8 @@
+let timerInterval;
+ let initialSeconds; 
+let seconds;
 function StartGame(){
+    let points=0;
     const mode = document.getElementById('mode').value;
     const color=document.getElementById('squareColor').value;
     let container = document.getElementById('settingsContainer');
@@ -7,6 +11,10 @@ function StartGame(){
             timerDiv.id = 'timer';
             timerDiv.textContent = 'Time: ';
             container.appendChild(timerDiv);
+    const score =document.createElement('div');
+          score.id='score';
+          score.textContent= ' Score 0 '
+          container.appendChild(score);
     let GameField=document.createElement('div');
     GameField.id='GameField';
     document.body.appendChild(GameField);
@@ -15,17 +23,23 @@ function StartGame(){
         square.id='square';
         square.style.backgroundColor=color;
         GameField.appendChild(square);
-
+        let squareSize;
         let seconds ;
         switch(mode){
-            case "EZ": seconds=10;
+            case "EZ": initialSeconds=10;
+            squareSize = 70;
             break;
-            case "Mid": seconds=5;
+            case "Mid": initialSeconds=5;
+            squareSize = 40;
             break;
-            case "Hard":seconds=1;
+            case "Hard":initialSeconds=2;
+            squareSize = 20;
             break;
 
         }
+        square.style.width = squareSize + 'px';
+        square.style.height = squareSize + 'px';
+        seconds = initialSeconds;
         timerDiv.textContent = `Time: ${seconds}`;
         const timerInterval = setInterval(() => {
             seconds--;
@@ -38,8 +52,10 @@ function StartGame(){
 
         }, 1000);
         square.addEventListener('click',()=>{
-            
-
+            seconds = initialSeconds;
+            points=points+10;
+             timerDiv.textContent = `Time: ${seconds}`;
+             score.textContent=`Score: ${points}`
             const squareWidth = square.offsetWidth;
             const squareHeight = square.offsetHeight;
             const maxX = GameField.offsetWidth - squareWidth;
